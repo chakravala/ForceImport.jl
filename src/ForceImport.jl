43687aae580c4ceb1e94bf48b6b4312539ec32c2
Michael Reed <michaelreed@crucialflow.com>
2018-04-30 11:01:02 -0400

    simplified implementation with names method

--- src/ForceImport.jl
+++ src/ForceImport.jl
@@ -6,38 +6,6 @@ module ForceImport
 
 export @force
 
-# adds exported items to import list
-function extend_list!(list,expr)
-    if typeof(expr) == Expr
-        for erg ∈ (expr.head == :block ? expr.args : [expr])
-            if typeof(erg) == Expr && erg.head == :export
-                push!(list,erg.args...)
-            end
-        end
-    end
-end
-
-# searches Julia module for exports
-function extend_find!(list,args,pkg)
-    for arg ∈ args
-        if typeof(arg) == Expr
-            if arg.head == :export
-                push!(list,arg.args...)
-            elseif (arg.head == :call && (arg.args[1] == :eval ||
-                    (arg.args[1] == :|> && arg.args[3] == :eval))) ||
-                    (arg.head == :macrocall && arg.args[1] == Symbol("@eval"))
-                extend_list!(list,arg.args[2])
-            elseif (arg.head == :call && arg.args[1] == :include)
-                nargs = nothing
-                open(joinpath(Pkg.dir(pkg),"src",arg.args[2])) do f
-                    nargs = "module foo\n"*readstring(f)*"\nend"
-                end
-                extend_find!(list,parse(nargs).args[3].args,pkg)
-            end
-        end
-    end
-end
-
 """
 
     @force using ModuleName
@@ -46,14 +14,13 @@ Forces imports of `ModuleName`'s exports, even if there are conflicts.
 """
 macro force(import_module)
     import_module.head ≠ :using && throw(error("not a using statement"))
-    pkg = string(import_module.args[1])
-    args = nothing
-    open(joinpath(Pkg.dir(pkg),"src","$pkg.jl")) do f
-        args = readstring(f)
+    pkgs = import_module.args
+    out = []
+    for pkg ∈ pkgs
+        s=:(Expr(:toplevel,[Expr(:import,Symbol($(string(pkg))),j) for j∈names($pkg)]...))
+        push!(out,:(import $pkg),:(eval($s)))
     end
-    list = []
-    extend_find!(list,parse(args).args[3].args,pkg)
-    return Expr(:toplevel,[Expr(:import,import_module.args[1],j) for j ∈ list]...)
+    return Expr(:block,out...)
 end
 
 __init__() = nothing
